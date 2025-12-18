884502569f7338e998ebfe70cf3b1f82d9c6cc67
vijeyender <vijaylearning26@gmail.com>
2025-12-18 22:51:23 +0530

    commit message

--- src/main/java/com/max/employee/EmployeeApplication.java
+++ src/main/java/com/max/employee/EmployeeApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
--- src/test/java/com/max/employee/EmployeeApplicationTests.java
+++ src/test/java/com/max/employee/EmployeeApplicationTests.java
@@ -3,11 +3,29 @@ package com.max.employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class EmployeeApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void calculatorAddsTwoNumbers() {
+		EmployeeApplication app = new EmployeeApplication();
+		assertEquals(5, app.calculator(2, 3));
+	}
 
+	@Test
+	void calculatorHandlesNegativeNumbers() {
+		EmployeeApplication app = new EmployeeApplication();
+		assertEquals(-1, app.calculator(2, -3));
+	}
+
+	@Test
+	void calculatorWithZero() {
+		EmployeeApplication app = new EmployeeApplication();
+		assertEquals(3, app.calculator(3, 0));
+	}
 }
