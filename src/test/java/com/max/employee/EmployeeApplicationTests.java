dd338845f31f9066fdb7222dd146ac9d9c146180
vijeyender <vijaylearning26@gmail.com>
2025-12-18 22:51:51 +0530

    commit message

--- src/test/java/com/max/employee/EmployeeApplicationTests.java
+++ src/test/java/com/max/employee/EmployeeApplicationTests.java
@@ -11,21 +11,4 @@ class EmployeeApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	void calculatorAddsTwoNumbers() {
-		EmployeeApplication app = new EmployeeApplication();
-		assertEquals(5, app.calculator(2, 3));
-	}
-
-	@Test
-	void calculatorHandlesNegativeNumbers() {
-		EmployeeApplication app = new EmployeeApplication();
-		assertEquals(-1, app.calculator(2, -3));
-	}
-
-	@Test
-	void calculatorWithZero() {
-		EmployeeApplication app = new EmployeeApplication();
-		assertEquals(3, app.calculator(3, 0));
-	}
 }
